dccd8312927692483ec679ee5d69d62fbb143b2c
amanbedi18 <amanbedi93@gmail.com>
2018-04-14 01:34:57 +0530

    Add files via upload

--- UploadCertificatesToKV.ps1
+++ UploadCertificatesToKV.ps1
@@ -10,31 +10,7 @@
     [Parameter(ParameterSetName='Customize',Mandatory=$true)]
     [Parameter(ParameterSetName='Secondary',Mandatory=$true)]
 	[String]
-    $kvCertPassword,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $dpClientCertPath,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $dpClientCertPassword
-<#    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $apCertPath,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $apCertPassword,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $vtCertPath,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $vtCertPassword,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $dpCertPath,
-    [Parameter(ParameterSetName='Customize',Mandatory=$true)]
-	[String]
-    $dpCertPassword,#>
+    $kvCertPassword
 )
 
 
@@ -71,41 +47,3 @@ $certurl = uploadCert -certPath $kvCertPath -certPassword $kvCertPassword -secre
 Write-Host ("##vso[task.setvariable variable=KVCertURL;]$certurl")
 Write-Host $env:KVCertURL
 Write-Host "Certificate URL : "$certurl
-
-
-#------------------------------   Device Provisining Client Certificate    -------------------------------#
-
-if($PSCmdlet.ParameterSetName -ne 'Secondary'){
-
-$certurl = uploadCert -certPath $dpClientCertPath -certPassword $dpClientCertPassword -secretName "DPClientCertificate"
-
-Write-Host ("##vso[task.setvariable variable=DPClientCertURL;]$certurl")
-Write-Host $env:DPClientCertURL
-Write-Host "Certificate URL : "$certurl
-
-}
-
-<#
-#------------------------------   Admin Portal Certificate    -------------------------------#
-
-$certurl = uploadCert -certPath $apCertPath -certPassword $apCertPassword -secretName "AdminPortalCertificate"
-
-Write-Host ("##vso[task.setvariable variable=APCertURL;]$certurl")
-Write-Host $env:APCertURL
-Write-Host "Certificate URL : "$certurl
-
-#------------------------------   Vehicle Tracking Certificate    -------------------------------#
-
-$certurl = uploadCert -certPath $vtCertPath -certPassword $vtCertPassword -secretName "VehicleTrackingCertificate"
-
-Write-Host ("##vso[task.setvariable variable=VTCertURL;]$certurl")
-Write-Host $env:VTCertURL
-Write-Host "Certificate URL : "$certurl
-
-#------------------------------   Device Provisioning Certificate    -------------------------------#
-
-$certurl = uploadCert -certPath $dpCertPath -certPassword $dpCertPassword -secretName "DeviceProvisiningCertificate"
-
-Write-Host ("##vso[task.setvariable variable=DPCertURL;]$certurl")
-Write-Host $env:DPCertURL
-Write-Host "Certificate URL : "$certurl#>
